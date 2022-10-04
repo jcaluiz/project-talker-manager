9b5285dacc90ad95c6833f0ef0050e7d29672dd5
Luiz Junior <lacjunior129.la@gmail.com>
2022-10-04 15:30:10 -0300

    requisito 1 concluído

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -14,4 +18,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const pathTalkers = path.resolve(__dirname, 'talker.json');
+  const talkers = JSON.parse(await fs.readFile(pathTalkers));
+  res.status(200).json(talkers);
 });
